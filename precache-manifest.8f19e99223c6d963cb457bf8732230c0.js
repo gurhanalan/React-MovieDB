7692f41dd7968016a131441317f5fbc18feb338b
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-06-27 02:13:58 +0300

    Updates

---
+++ precache-manifest.8f19e99223c6d963cb457bf8732230c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3428a56465a4d8dfc3680a7a88f0910",
+    "url": "/React-MovieDB/index.html"
+  },
+  {
+    "revision": "806466a3a661922e1c78",
+    "url": "/React-MovieDB/static/css/main.88edc8de.chunk.css"
+  },
+  {
+    "revision": "2b103926d50271c4245d",
+    "url": "/React-MovieDB/static/js/2.b996dd61.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/React-MovieDB/static/js/2.b996dd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806466a3a661922e1c78",
+    "url": "/React-MovieDB/static/js/main.19aee299.chunk.js"
+  },
+  {
+    "revision": "01c2999282fdae9f9d76",
+    "url": "/React-MovieDB/static/js/runtime-main.93ca6d40.js"
+  }
+]);
